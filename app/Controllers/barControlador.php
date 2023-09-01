ceca3d2db8ffa4c2793a3414d1597505e73d279d
Lukeneitor <130513741+Lukeneitor@users.noreply.github.com>
2023-09-01 18:23:05 -0300

    1/9
    sfs

--- app/Controllers/barControlador.php
+++ app/Controllers/barControlador.php
@@ -1,20 +1,32 @@
 <?php
 
 namespace App\Controllers;
-
+use App\Models\TipoBebidaModelo;
 use App\Models\BebidaModelo;
 use CodeIgniter\Controller;
 
 class BarControlador extends Controller
 {
     public function index()
-    {   
-        echo view ('comunes/header');
-        // Obtener todas las bebidas disponibles
+    {
         $bebidaModelo = new BebidaModelo();
         $data['bebidas'] = $bebidaModelo->findAll();
 
+        // Obtener los tipos de bebida disponibles
+
+        //$tiposBebida = $bebidaModelo->distinct('tipo_id')->findColumn('tipo_id');
+        $tipoBebidaModelo = new TipoBebidaModelo();
+        $data['tiposBebida'] = $tipoBebidaModelo->findAll();
+
         return view('barVista', $data);
+    }
+
+    public function bebidasPorTipo($tipoId)
+    {
+        $tipoBebidaModelo = new TipoBebidaModelo();
+        $bebidasPorTipo = $tipoBebidaModelo->getBebidasPorTipo($tipoId);
+
+  
     }
 
     public function buscarBebida()
@@ -43,6 +55,28 @@ class BarControlador extends Controller
 
         return view('barVista', $data);
     }
-    
+    public function filtrarPorTipo($tipo)
+{
+    // Obtener las bebidas filtradas por tipo
+    $bebidaModelo = new BebidaModelo();
+    $data['bebidas'] = $bebidaModelo->where('tipo_id', $tipo)->findAll();
 
+    // Obtener los tipos de bebida disponibles
+    $tipoBebidaModelo = new TipoBebidaModelo();
+    $data['tiposBebida'] = $tipoBebidaModelo->findAll();
+
+    return view('barVista', $data);
+}
+    public function login(){
+        return view('loginVista');
+    }
+    public function paginaRegistroExitoso()
+{
+    return view('registro_exitoso'); // Cambia 'registro_exitoso' por el nombre de tu vista
+}
+public function pruebadropdown()
+{
+    return view('pruebadropdown'); // Borrar despues de terminar pruebas.
+}
+    // Otros métodos del controlador, si es necesario
 }
--- app/Views/barVista.php
+++ app/Views/barVista.php
@@ -1,76 +1,102 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title>Bar</title>
-    <!-- Enlace al archivo CSS personalizado -->
-    <link rel="stylesheet" href="<?php echo base_url('css/barVista.css'); ?>">
-</head>
-
 <body>
-    <div class='position-relative'>
-        <header class="bg-dark text-white">
-            <div class="container py-3">
-                <h1>CryptoCafé</h1>
+    <header class="bg-dark text-white py-3">
+        <div class="container d-flex justify-content-between align-items-center">
+            <img src="<?php echo base_url("/assets/images/logo.jpg"); ?>" alt=""
+                class="img-fluid rounded-square" style="width: 100px; height: 100px;">
+            <h1 class="mb-0">ByTender</h1>
+            <a class="inicio" href="<?php echo base_url('registro'); ?>">Registrarse</a>
+        </div>
+        <form action="<?php echo base_url('barControlador/buscarBebida'); ?>" method="post"
+            class="form-inline formulario">
+            <div class="container">
+                <div class="input-group">
+                    <input type="text" name="busqueda" placeholder="Buscar bebida favorita" class="form-control">
+                    <button class="btn btn-primary" type="submit"><i class="bi bi-search"></i></button>
+                </div>
             </div>
-            <form action="<?php echo base_url('barControlador/buscarBebida'); ?>" method="post" class="form-inline">
-                <input type="text" name="busqueda" placeholder="Buscá tu bebida favorita" class="form-control mr-2">
-                <input type="submit" value="Buscar" class="btn btn-primary">
-            </form>
-    </div>
-    <!DOCTYPE html>
-    <html lang="en"> 
-    
+        </form>
+    </header>
 
-    
-
-    </html>
-    
-    
-
-            </section>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="<?php echo base_url(); ?>">INICIO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <?php if (!empty($tiposBebida)) : ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Filtrar por tipo
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <?php foreach ($tiposBebida as $tipo) : ?>
+                            <li><a class="dropdown-item"
+                                    href="<?php echo base_url('barControlador/filtrarPorTipo/' . $tipo['id']); ?>"><?php echo $tipo['nombre']; ?></a>
+                            </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
         </div>
     </nav>
+
     <div class="container py-4">
-        <?php if (isset($bebidaEncontrada)) : ?>
-            <?php if (is_array($bebidaEncontrada)) : ?>
-                <section class="mt-4">
-                    <div class="bebida-encontrada">
-                        <h2>Información de la bebida:</h2>
-                        <div class="bebida-item">
-                            <ul class="list-group">
-                                <?php foreach ($bebidaEncontrada as $bebida) : ?>
-                                    <li class="list-group-item"><strong>Nombre:</strong> <?php echo $bebida['nombre']; ?></li>
-                                    <li class="list-group-item"><strong>Tipo:</strong> <?php echo $bebida['tipo']; ?></li>
-                                    <li class="list-group-item"><strong>Precio:</strong> <?php echo $bebida['precio']; ?></li>
-                                    <li class="list-group-item"><strong>Descripción:</strong> <?php echo $bebida['descripcion']; ?></li>
-                                    <li class="list-group-item"><strong>Ingredientes:</strong> <?php echo $bebida['ingredientes']; ?></li>
-                                <?php endforeach; ?>
-                            </ul>
+    <?php if (isset($bebidaEncontrada)) : ?>
+        <section class="mt-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">Información de la bebida:</h2>
+                    <ul class="list-group list-group-flush">
+                        <?php foreach ($bebidaEncontrada as $bebida) : ?>
+                            <li class="list-group-item"><strong>Nombre:</strong> <?php echo $bebida['nombre']; ?></li>
+                            <li class="list-group-item"><strong>Tipo:</strong> <?php echo $bebida['tipo_id']; ?></li>
+                            <li class="list-group-item"><strong>Precio:</strong> <?php echo $bebida['precio']; ?></li>
+                            <li class="list-group-item"><strong>Descripción:</strong> <?php echo $bebida['descripcion']; ?></li>
+                            <li class="list-group-item">
+                                <img src="<?php echo base_url(); ?>assets/images/<?php echo $bebida['imagen_ruta']; ?>" alt="Imagen de la bebida" style="max-width: 100%; height: auto;">
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    <?php elseif (isset($bebidas) && is_array($bebidas)) : ?>
+        <section class="mt-4">
+            <h2 class="mb-3">Bebidas Disponibles:</h2>
+            <div class="row">
+                <?php foreach ($bebidas as $bebida) : ?>
+                    <div class="col-md-4">
+                        <div class="card mb-4 shadow-sm">
+                            <img src="<?php echo base_url(); ?>assets/images/<?php echo $bebida['imagen_ruta']; ?>" alt="Imagen de la bebida" class="card-img-top" style="max-height: 200px; object-fit: cover;">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $bebida['nombre']; ?></h5>
+                                <p class="card-text"><strong>Tipo:</strong> <?php echo $bebida['tipo_id']; ?></p>
+                                <p class="card-text"><strong>Precio:</strong> <?php echo $bebida['precio']; ?></p>
+                                <p class="card-text"><strong>Descripción:</strong> <?php echo $bebida['descripcion']; ?></p>
+                            </div>
                         </div>
                     </div>
-                </section>
-            <?php else : ?>
-                <div class="alert alert-info mt-4">No se encontraron bebidas con ese nombre.</div>
-            <?php endif; ?>
-        <?php elseif (isset($bebidas) && is_array($bebidas)) : ?>
-            <section class="mt-4">
-                <h2>Bebidas Disponibles:</h2>
-                <div class="bebidas-container">
-                    <?php foreach ($bebidas as $bebida) : ?>
-                        <div class="bebida-item">
-                            <strong>Nombre:</strong> <?php echo $bebida['nombre']; ?>
-                            <strong>Tipo:</strong> <?php echo $bebida['tipo']; ?>
-                            <strong>Precio:</strong> <?php echo $bebida['precio']; ?>
-                            <strong>Descripción:</strong> <?php echo $bebida['descripcion']; ?>
-                            <strong>Ingredientes:</strong> <?php echo $bebida['ingredientes']; ?>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-            </section>
-            <a href="<?php echo base_url('adminBebidas/agregar'); ?>" class="boton">Agregar Bebida</a>
-        <?php endif; ?>
-    </div>
+                <?php endforeach; ?>
+            </div>
+        </section>
+    <?php endif; ?>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.2/dist/umd/popper.min.js"
+        integrity="sha384-q9CRHqZndzlxGLOj+xrdLDJa9ittGte1NksRmgJKeCV9DrM7Kz868XYqsKWPpAmn"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
