73f5091e8fe38c8fa3f4910ad64b3b82f091877f
anachuchu278 <130513591+anachuchu278@users.noreply.github.com>
2023-10-04 17:41:46 -0300

    login

--- app/Controllers/loginControlador.php
+++ app/Controllers/loginControlador.php
@@ -8,29 +8,34 @@ use App\Models\LoginModelo;
 class loginControlador extends BaseController
 {
     public function Index()
-    {   
+    {
         echo view('comunes/header');
         return view('loginVista');
-    } 
-    public function loguearse(){
-        $LoginModelo = new LoginModelo(); 
-        
-        $email = $this->request->getPost('email');
-        $password = $this->request->getPost('contrasena');
-
-        $result = $LoginModelo->where('email',$email)->first(); 
-        if ($result['id']> 0){
-            if (password_verify($password, $result['contrasena'])) {
-                //$this->session->set("user",$result); 
-
-                echo view('crud');
-            } 
-            else {
-                echo 'Invalid password.';
-            }
-        }
-
     }
+    public function loguearse()
+{
+    $LoginModelo = new LoginModelo();
 
+    $email = $this->request->getPost('email');
+    $password = $this->request->getPost('contrasena');
+    
+    $result = $LoginModelo->where('email', $email)->first();
+
+    if ($result) {
+        // Verificar si $result no es vacío antes de acceder a sus propiedades
+        if ($result['id'] > 0 && password_verify($password, $result['contrasena'])) {
+            $this->session->set("usuario", $result);
+
+            return redirect()->to("/crud");
+        } else {
+            echo 'La contraseña no es correcta';
+        }
+    } else {
+        echo 'El email no se encuentra registrado';
+    }
+    }
+    public function crud(){
+        return view('crud');
+    }
 
 }
